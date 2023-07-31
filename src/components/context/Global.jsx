0e6875423a4aed8db52a46648c0c703fd24c09fd
Tautvydas Z <tautzuk@tutanota.com>
2023-08-01 01:14:23 +0300

    Create Global context

---
+++ src/components/context/Global.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState } from "react";
+import { getWeatherData } from "../../utils/getWeatherData.js";
+
+export const Global = createContext();
+
+export const GlobalProvider = ({ children }) => {
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [weatherVariables, setWeatherVariables] = useState({});
+    let [weatherData, setWeatherData] = useState(null);
+
+    const weatherVariablesHandler = (e) => {
+        const collection = e.target.selectedOptions;
+        let values = {};
+        for (let i = 0; i < collection.length; i++) {
+            values[i] = collection[i].value;
+        }
+        setWeatherVariables(values);
+    };
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const data = await getWeatherData(startDate, endDate, weatherVariables);
+        setWeatherData(data);
+    };
+
+    return (
+        <Global.Provider
+            value={{
+                setStartDate,
+                startDate,
+                setEndDate,
+                endDate,
+                weatherVariablesHandler,
+                submitHandler,
+                weatherData,
+            }}>
+            {children}
+        </Global.Provider>
+    );
+};
