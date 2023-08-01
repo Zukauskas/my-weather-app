a6f8e0813a1daa009c64d18f109e23f36cf7ee11
Tautvydas Z <tautzuk@tutanota.com>
2023-08-01 18:07:36 +0300

    Update global for markers

--- src/components/context/Global.jsx
+++ src/components/context/Global.jsx
@@ -9,6 +9,7 @@ export const GlobalProvider = ({ children }) => {
     const [endDate, setEndDate] = useState("");
     const [weatherVariables, setWeatherVariables] = useState({});
     let [weatherData, setWeatherData] = useState(null);
+    const [markers, setMarkers] = useState([]);
 
     const weatherVariablesHandler = (e) => {
         const collection = e.target.selectedOptions;
@@ -21,7 +22,12 @@ export const GlobalProvider = ({ children }) => {
 
     const submitHandler = async (e) => {
         e.preventDefault();
-        const data = await getWeatherData(startDate, endDate, weatherVariables);
+        const data = await getWeatherData(
+            startDate,
+            endDate,
+            weatherVariables,
+            markers
+        );
         setWeatherData(data);
     };
 
@@ -35,6 +41,8 @@ export const GlobalProvider = ({ children }) => {
                 weatherVariablesHandler,
                 submitHandler,
                 weatherData,
+                markers,
+                setMarkers,
             }}>
             {children}
         </Global.Provider>
