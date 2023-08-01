ef9c192f463114ad67a98b5d4eb2a191fe46f5ed
Tautvydas Z <tautzuk@tutanota.com>
2023-08-01 18:08:04 +0300

    Update for multiple markers

--- src/utils/getWeatherData.js
+++ src/utils/getWeatherData.js
@@ -1,18 +1,30 @@
-export const getWeatherData = async (start, end, variables) => {
-    let responseData = {};
-    const variablesArr = Object.values(variables);
-    let variableString = variablesArr.join(",");
+export const getWeatherData = async (start, end, variables, markers) => {
+    const weatherDataPromises = markers.map(async (marker) => {
+        const lat = marker.lat.toFixed(6);
+        const long = marker.lng.toFixed(6);
+        const variablesArr = Object.values(variables);
+        const variableString = variablesArr.join(",");
+
+        try {
+            const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=${variableString}&timezone=auto&start_date=${start}&end_date=${end}`;
+
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            const responseData = await response.json();
+            return responseData;
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            return null;
+        }
+    });
 
     try {
-        const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=54.9706&longitude=24.0618&hourly=${variableString}&timezone=auto&start_date=${start}&end_date=${end}`;
-
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        responseData = await response.json();
+        const weatherData = await Promise.all(weatherDataPromises);
+        return weatherData;
     } catch (error) {
         console.error("Error fetching data:", error);
+        return [];
     }
-    return responseData;
 };
