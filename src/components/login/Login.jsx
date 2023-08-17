f6e924f33474be965a60057d88f7d36a0d682b49
Tautvydas Zukauskas <tautzuk@tutanota.com>
2023-08-17 23:30:37 +0300

    Create login component

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { Global } from "../context/Global.jsx";
+import "./Login.css";
+
+export default function Login() {
+    const { login } = useContext(Global);
+
+    return (
+        <div className="login-container">
+            <p className="login-message">
+                You are not signed in. Please sign in with Google to access the
+                app&apos;s features.
+            </p>
+            <button className="login-button" onClick={() => login()}>
+                Sign in with Google
+            </button>
+        </div>
+    );
+}
