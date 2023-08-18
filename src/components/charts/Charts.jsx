f099ef12e99ede3fca7bb6dfc69b2c93dc68802d
Tautvydas Zukauskas <tautzuk@tutanota.com>
2023-08-18 12:58:30 +0300

    Memoize data to reduce rerenders of component

--- src/components/charts/Charts.jsx
+++ src/components/charts/Charts.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/display-name */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-prototype-builtins */
-import { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import { Global } from "../context/Global.jsx";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
@@ -13,8 +15,17 @@ const getRandomColor = () => {
     return `rgb(${r}, ${g}, ${b})`;
 };
 
+const Chart = React.memo(({ chartOptions }) => {
+    return (
+        <div className="chart">
+            <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+        </div>
+    );
+});
+
 const Charts = () => {
     const { weatherData } = useContext(Global);
+
     if (!weatherData) {
         return (
             <div className="charts">
@@ -24,95 +35,97 @@ const Charts = () => {
         );
     }
 
-    const charts = weatherData.map((data, index) => {
-        const labels = data.hourly.time;
-        const datasets = [];
-        const uniqueUnits = {};
-
-        const chartOptions = {
-            chart: {
-                type: "line",
-            },
-            title: {
-                text: `Marker ${index + 1}`,
-            },
-            xAxis: {
-                type: "datetime",
-                categories: labels,
-                labels: {
-                    step: 24,
-                    formatter: function () {
-                        let label = this.value.substring(0, 10);
-                        return label;
-                    },
-                },
-            },
-            yAxis: [],
-            series: datasets,
-            plotOptions: {
-                line: {
-                    lineWidth: 3,
-                },
-            },
-            legend: {
-                enabled: true,
-                layout: "vertical",
-                align: "right",
-                verticalAlign: "middle",
-            },
-            tooltip: {
-                shared: true,
-                backgroundColor: "rgba(255, 255, 255, 0.85)",
-                borderColor: "#999999",
-                borderRadius: 5,
-                borderWidth: 1,
-            },
-            credits: {
-                enabled: false,
-            },
-        };
-
-        for (const variable in data.hourly) {
-            if (data.hourly.hasOwnProperty(variable) && variable !== "time") {
-                const dataPoints = data.hourly[variable];
-
-                const unit = data.hourly_units[variable] || "";
-                const axisIndex = uniqueUnits[unit];
-
-                if (typeof axisIndex === "undefined") {
-                    const yAxis = {
-                        title: {
-                            text: unit,
-                        },
-                    };
-                    uniqueUnits[unit] = datasets.length;
-                    datasets.push({
-                        name: variable,
-                        data: dataPoints,
-                        color: getRandomColor(),
-                        yAxis: datasets.length,
-                    });
-                    chartOptions.yAxis.push(yAxis);
-                } else {
-                    datasets.push({
-                        name: variable,
-                        data: dataPoints,
-                        color: getRandomColor(),
-                        yAxis: axisIndex,
-                    });
-                }
-            }
+    const charts = useMemo(() => {
+        if (!weatherData) {
+            return [];
         }
 
-        return (
-            <div key={index} className="chart">
-                <HighchartsReact
-                    highcharts={Highcharts}
-                    options={chartOptions}
-                />
-            </div>
-        );
-    });
+        return weatherData.map((data, index) => {
+            const labels = data.hourly.time;
+            const datasets = [];
+            const uniqueUnits = {};
+
+            const chartOptions = {
+                chart: {
+                    type: "line",
+                },
+                title: {
+                    text: `Marker ${index + 1}`,
+                },
+                xAxis: {
+                    type: "datetime",
+                    categories: labels,
+                    labels: {
+                        step: 24,
+                        formatter: function () {
+                            let label = this.value.substring(0, 10);
+                            return label;
+                        },
+                    },
+                },
+                yAxis: [],
+                series: datasets,
+                plotOptions: {
+                    line: {
+                        lineWidth: 3,
+                    },
+                },
+                legend: {
+                    enabled: true,
+                    layout: "vertical",
+                    align: "right",
+                    verticalAlign: "middle",
+                },
+                tooltip: {
+                    shared: true,
+                    backgroundColor: "rgba(255, 255, 255, 0.85)",
+                    borderColor: "#999999",
+                    borderRadius: 5,
+                    borderWidth: 1,
+                },
+                credits: {
+                    enabled: false,
+                },
+            };
+
+            for (const variable in data.hourly) {
+                if (
+                    data.hourly.hasOwnProperty(variable) &&
+                    variable !== "time"
+                ) {
+                    const dataPoints = data.hourly[variable];
+
+                    const unit = data.hourly_units[variable] || "";
+                    const axisIndex = uniqueUnits[unit];
+
+                    if (typeof axisIndex === "undefined") {
+                        const yAxis = {
+                            title: {
+                                text: unit,
+                            },
+                        };
+                        uniqueUnits[unit] = datasets.length;
+                        datasets.push({
+                            name: variable,
+                            data: dataPoints,
+                            color: getRandomColor(),
+                            yAxis: datasets.length,
+                        });
+                        chartOptions.yAxis.push(yAxis);
+                    } else {
+                        datasets.push({
+                            name: variable,
+                            data: dataPoints,
+                            color: getRandomColor(),
+                            yAxis: axisIndex,
+                        });
+                    }
+                }
+            }
+
+            return <Chart key={index} chartOptions={chartOptions} />;
+        });
+    }, [weatherData]);
 
     return (
         <div className="charts">
